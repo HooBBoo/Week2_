72f2171cd59433426cdd221b05f53f774ec0fd18
이지윤 <wldbs272@naver.com>
2024-10-24 12:45:01 +0900

    Q2_2

--- Assets/Scripts/ScoreButton.cs
+++ Assets/Scripts/ScoreButton.cs
@@ -1,25 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Xml.Serialization;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class ScoreButton : MonoBehaviour
 {
     public Button scoreButton;
     public int score = 0;
     public Text scoreText;
+    public Action<int> OnScoreChanged;
 
     void Awake()
     {
         scoreButton = GetComponent<Button>();
         scoreButton.onClick.AddListener(PointUp); // 메소드를 연결할 때는 () 안붙임.
-        RefreshUI();
     }
 
     void PointUp()
     {
-        score++;
+        score += 1;
         Debug.Log("Score" + score);
+        OnScoreChanged?.Invoke(score);
         RefreshUI();
     }
 
---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ScoreText : MonoBehaviour
+{
+    public ScoreButton scoreButton;
+    public TextMeshProUGUI scoreText;
+
+    void Awake()
+    {
+        scoreButton.OnScoreChanged += RefreshUI; // OnScoreChanged가 점수가 변경된 것 받아와서
+                                                 // RefreshUI가 실행
+    }
+    void RefreshUI(int newScore)
+    {
+        scoreText.text = "Score: " + newScore;
+    }
+}
